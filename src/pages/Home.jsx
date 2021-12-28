f517c9a90e7a2f01b319c365647e83e209b064a0
Chirag <56735482+Chirag018@users.noreply.github.com>
2021-12-28 09:56:11 +0530

    blog is ready to be used,but ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { collection, deleteDoc, getDocs, doc } from 'firebase/firestore'
+import React, { useState, useEffect } from 'react'
+import { auth, db } from '../firebase-config'
 
-export default function Home() {
+export default function Home({ isAuth }) {
+
+    const [postLists, setPostLists] = useState([])
+    const postsCollectionRef = collection(db, 'posts')
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollectionRef);
+            setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        };
+
+        getPosts();
+    })
+
+    const deletePost = async (id) => {
+        const postDoc = doc(db, 'posts', id);
+        await deleteDoc(postDoc);
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='homePage'>
+            {postLists.map((post) => {
+                return (
+                    <div className="post">
+                        <div className="postHeader">
+                            <div className="title">
+                                <h1>{post.title}</h1>
+                            </div>
+                            <div className="deletePost">
+                                {isAuth && post.author.id === auth.currentUser.uid && (
+                                    <button onClick={() => {
+                                        deletePost(post.id)
+                                    }}> &#128465;</button>
+                                )}
+                            </div>
+                        </div>
+                        <div className="postTextContainer">
+                            {post.postText}
+                            <h3>@{post.author.name}</h3>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { auth, provider } from '../firebase-config'
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
-export default function Login() {
+export default function Login({setIsAuth}) {
+   
+   let navigate=useNavigate();
+
+    const signInWithGoogle = () => {
+     signInWithPopup(auth,provider).then((result)=>{
+         localStorage.setItem('isAuth',true);
+         setIsAuth(true);
+         navigate('/')
+     })
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='loginPage'>
+            <p>Sign in with Google</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>Sign in</button>
         </div>
     )
 }
